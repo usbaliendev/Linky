55826aaf67ed4cd548c09a3a1da2eaa40f74d9f9
usbalien.dev <usbaliendev@amail.com>
2024-10-10 20:40:03 -0300

    feat: add categories component

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -4,12 +4,17 @@ import { styles } from "./styles";
 import { categories } from "@/utils/categories";
 import { Category } from '@/components/category';
 
-export function Categories(){
+type CategoriesProps = {
+  selected: string
+  onChange: (category: string) => void
+}
+export function Categories({ selected, onChange }: CategoriesProps) {
   return (
     <FlatList
       data={categories}
       keyExtractor={(item)=> item.id}
-      renderItem={({item}) => <Category name={item.name} icon={item.icon} isSelected={false}/>}
+      renderItem={({item}) => <Category name={item.name} icon={item.icon} isSelected={item.name === selected}
+      onPress={() => onChange(item.name)}/>}
       horizontal
       style={styles.container}
       contentContainerStyle={styles.content}
